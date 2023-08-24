ebecc480519c22d369ef0d411b304ee006ed10b5
Shiv-OM <116488936+Shiv-OM@users.noreply.github.com>
2023-08-24 16:18:04 +0530

    Update Friend.jsx

--- client/src/componets/Friend.jsx
+++ client/src/componets/Friend.jsx
@@ -26,7 +26,7 @@ const Friend = ({ friendId, name, userPicturePath }) => {
   const patchFriend = async () => {
    
     const response = await fetch(
-      `http://localhost:3001/users/${_id}/${friendId}`,
+      `${process.env.REACT_APP_BACKEND}/users/${_id}/${friendId}`,
       {
         method: "PATCH",
         headers: {
